d7ab0150e94228ace9cce91837316d759c777f5f
Mwansa <Mwansad.mwango@gmail.com>
2021-04-26 02:45:55 +0800

    Flask api mongoDB to extract racechart data complete

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify
+import pprint
+import time
 # Import our pymongo library, which lets us connect our Flask app to our Mongo database.
 from flask_pymongo import PyMongo
 # import scrape_telsa_data TBC - may not be required since data is already loaded in MongoDB
@@ -10,27 +12,49 @@ app = Flask(__name__)
 mongo = PyMongo(app, uri="mongodb://localhost:27017/electric_vehicles")
 
 # Query DB for all relevant data to be used to initialise pages
-tesla_prod_sales_data = mongo.db.tesla_production_sales.find({})
-print("Tesla data", tesla_prod_sales_data)
-us_ev_sales_data = mongo.db.us_ev_sales.find({})
-print("US EV Sales data", us_ev_sales_data)
+tesla_prod_sales_coll = mongo.db.tesla_production_sales.find({})
+# print("Tesla data", tesla_prod_sales_obj)
+# for document in tesla_prod_sales_obj: pprint.pprint(document)
 
-# Set route - displays first entry in MongoDB
+     
+
+#################################################
+# Flask Routes
+#################################################
+
+# Set route - displays landing page
 @app.route("/")
 def index():
-    return render_template("index.html", tesla_prod_sales_data=tesla_prod_sales_data)
+    return render_template("index.html", tesla_prod_sales_data=tesla_prod_sales_coll)
 
-# Scrape page and store info in MongoDB database
-# @app.route("/")
-# def scrape():
-#     # Get mars data returned in results dictionary after running the scrape() function
-#     mars_data = scrape_mars.scrape()
-#     # Create and update table caled 'collection' in mission_to_mars_db
-#     mongo.db.collection.update({}, mars_data, upsert=True)
-#     print("Scraped!", mars_data)
-#     # Redirect back to home page
-#     return redirect("/")
+# Set route - displays race chart
+@app.route("/race_chart")
+def raceChart():
+    return render_template("race_chart.html")
 
+# Get EV Sales data from MongoDB database
+@app.route("/api/us-sales")
+def getUsSales():
+    all_sales = {}
+  # After you first set of iterations over documents the cursor is used up. It's a read-once container.
+  # Convert to list to avoid this.
+    us_ev_sales_coll = list(mongo.db.us_ev_sales.find({}))
+    # time.sleep(2)
+
+    for year in range(2011,2020):
+        vehicle_sales_dict_list = []
+        for document in us_ev_sales_coll: 
+            print("YEAR",year)  
+            if document['Vehicle'] != 'Total':
+                vehicle_sales_dict = {}
+                vehicle_sales_dict["vehicle"] = document['Vehicle']
+                vehicle_sales_dict["sales"] = document[str(year)]
+                vehicle_sales_dict_list.append(vehicle_sales_dict)
+        all_sales[year] = vehicle_sales_dict_list
+
+    return jsonify(all_sales) 
+
+    # return render_template("race_chart.html", us_ev_sales_data=us_ev_sales_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
