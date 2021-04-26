5c265323b90437ba21d8306949a760cd238fde2d
bentleyj68 <bentleyj68@gmail.com>
2021-04-26 17:31:58 +0800

    Electricity Production First Plotly Graph

--- api/api.py
+++ api/api.py
@@ -1,6 +1,9 @@
+# API Engine for the Electric Vehicles data project
+
 import flask
 from flask import request, jsonify
 from flask_pymongo import PyMongo
+import sys
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -8,55 +11,59 @@ app.config["DEBUG"] = True
 # Use flask_pymongo to set up mongo connection
 mongo = PyMongo(app, uri="mongodb://localhost:27017/electric_vehicles")
 
-
+# Default route - display helpful information
 @app.route('/', methods=['GET'])
 def home():
     return '''<h1>Electric Vehicles API</h1>
-<p>A prototype API for the Electric Vehicles project.</p>'''
+<p>An API engine for the Electric Vehicles project.</p>
+<br><p>API List'''
 
 
-# A route to return all of the available entries in our catalog.
+# A route to return all of the available copuntry codes in our catalogue.
 @app.route('/api/v1/resources/countries/all', methods=['GET'])
 def api_countries_all():
     docs = []
+    # read records from Mongo, remove the _id field, convert to JSON and allow for CORS
     for doc in mongo.db.country_codes.find():
         doc.pop('_id') 
         docs.append(doc)
-    return jsonify(docs)
+    response = jsonify(docs)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
     
-
+# A route to return all of the available electricy production values in our catalogue.
 @app.route('/api/v1/resources/electricity_production_values/all', methods=['GET'])
 def api_pv_all():
     docs = []
+    # read records from Mongo, remove the _id field, convert to JSON and allow for CORS
     for doc in mongo.db.electricity_production_values.find():
         doc.pop('_id') 
         docs.append(doc)
-    return jsonify(docs)
+    response = jsonify(docs)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @app.route('/api/v1/resources/electricity_production_values/country', methods=['GET'])
 def api_id():
-    # Check if an ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
+    # Check if a Country Code was provided as part of the URL.
+    # If ID is provided, lookup the Country Name from Countries collection
     # If no ID is provided, display an error in the browser.
     if 'id' in request.args:
-        id = str(request.args['id'])
+        country_code = mongo.db.country_codes.find({"Code": str(request.args['id'])})
+        print(country_code[0]['Name'], file=sys.stderr)
+        id = country_code[0]['Name']       
     else:
-        return "Error: No id field provided. Please specify an id."
+        return "Error: No id field provided. Please specify a country id."
 
-    # Loop through the data and match results that fit the requested ID.
-    # IDs are unique, but other fields might return many results
+    # read records from Mongo, remove the _id field, convert to JSON and allow for CORS
     docs = []
     for doc in mongo.db.electricity_production_values.find():
         doc.pop('_id') 
         if doc['country'] == id:
             docs.append(doc)
 
-    # Use the jsonify function from Flask to convert our list of
-    # Python dictionaries to the JSON format.
     response = jsonify(docs)
-    
     response.headers.add('Access-Control-Allow-Origin', '*')
-    
     return response
 
 
--- static/js/electricity_production_plots.js
+++ static/js/electricity_production_plots.js
@@ -1,74 +1,103 @@
-var country = "Australia";
-
 /* global Plotly */
+var country = "SE";
+var source = ['Oil, gas and coal', 'Renewable sources, excluding hydroelectric', 'Nuclear', 'Natural Gas', 'Hydroelectric'];
+var year1 = []; var year2 = []; var year3 = []; var year4 = []; var year5 = [];
+var percentages1 = []; percentages2 = []; percentages3 = []; percentages4 = []; percentages5 = [];
+
 var url =
   `http://localhost:5000/api/v1/resources/electricity_production_values/country?id=${country}`;
 
-/**
- * Helper function to select data
- * Returns an array of values
- * @param {array} rows
- * @param {integer} index
- * index 0 - country
- * index 1 - index
- * index 2 - percentage
- * index 3 - source_id
- * index 4 - year
- */
-function unpack(rows, index) {
-  return rows.map(function(row) {
-    return row[index];
-  });
-}
 
 function buildPlot() {
   d3.json(url).then(function(data) {
-    
-    var example = ['test', 'test2']
-    var country = data.country;
-    var index = data.index;
-    var percentage = data.percentage;
-    var source_id = data.source_id;
-    var year = data.year;
-        
+
+    var country = data[0].country;
+            
     // Grab values from the data json object to build the plots
     for (var i = 0; i < data.length; i++) {
-      example.push(data[i].percentage)
-    //   country.push(data[i].country);
-    //   index.push(data[i].index);
-    //   percentage.push(data[i].percentage);
-    //   source_id.push(data[i].source_id);
-    //   year.push(data[i].year);
+      
+      switch(data[i].source_id) {
+        case "EG.ELC.FOSL.ZS":
+          percentages1.push(data[i].percentage)
+          year1.push(data[i].year);
+          break;
+        case "EG.ELC.RNWX.ZS":
+          percentages2.push(data[i].percentage)
+          year2.push(data[i].year);
+          break;
+        case "EG.ELC.NUCL.ZS":
+          percentages3.push(data[i].percentage)
+          year3.push(data[i].year);
+          break;
+        case "EG.ELC.NGAS.ZS":
+          percentages4.push(data[i].percentage)
+          year4.push(data[i].year);
+          break;
+        case "EG.ELC.HYRO.ZS":
+          percentages5.push(data[i].percentage)
+          year5.push(data[i].year);
+          break;
+        default:
+          var message = "Unknown";
+      }
+      
     }
 
-    console.log(example);
+    console.log("here");
 
-    // var trace1 = {
-    //   type: "scatter",
-    //   mode: "lines",
-    //   name: name,
-    //   x: dates,
-    //   y: closingPrices,
-    //   line: {
-    //     color: "#17BECF"
-    //   }
-    // };
+    var trace1 = {
+      mode: "lines",
+      name: source[0],
+      x: year1,
+      y: percentages1,
+      line: {
+        color: "#17BECF"
+      }
+    };
 
-    // var data = [trace1];
+    var trace2 = {
+      mode: "lines",
+      name: source[1],
+      x: year2,
+      y: percentages2,
+    };
 
-    // var layout = {
-    //   title: `${stock} closing prices`,
-    //   xaxis: {
-    //     range: [startDate, endDate],
-    //     type: "date"
-    //   },
-    //   yaxis: {
-    //     autorange: true,
-    //     type: "linear"
-    //   }
-    // };
+    var trace3 = {
+      mode: "lines",
+      name: source[2],
+      x: year3,
+      y: percentages3,
+    };
 
-    // Plotly.newPlot("plot", data, layout);
+    var trace4 = {
+      mode: "lines",
+      name: source[3],
+      x: year4,
+      y: percentages4,
+    };
+
+    var trace5 = {
+      mode: "lines",
+      name: source[4],
+      x: year5,
+      y: percentages5,
+    };
+
+    var datatr = [trace1, trace2, trace3, trace4, trace5];
+
+    var layout = {
+      title: `${country} - Electricity Generation by Source (% of total)`,
+      xaxis: {
+        title: 'Year'
+      },
+      yaxis: {
+        autorange: true,
+        type: "linear",
+        title: 'Percentage'
+      }
+    };
+
+    Plotly.newPlot("plot", datatr, layout);
 
   });
 }
